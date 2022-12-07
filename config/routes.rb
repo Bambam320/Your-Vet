b1bb71961b2caac3b51e28928d0abc5955de9797
Bambam320 <igor.mestechkin@gmail.com>
2022-12-07 02:14:26 -0500

    Migrated tables, created associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :animals
+  resources :appointments
+  resources :doctors
   get '/hello', to: 'application#hello_world'
 
   get '*path',
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+add the data here first then build the front end
