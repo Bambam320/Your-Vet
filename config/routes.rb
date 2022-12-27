2a075999a27bfb8f874c796ab0604634c8c2945a
Bambam320 <igor.mestechkin@gmail.com>
2022-12-27 14:09:10 -0500

    update the seed file for issues during deployment

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   post '/login', to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+
   get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
